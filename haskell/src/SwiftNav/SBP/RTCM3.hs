cdd32f04db5e7db7e782a82ef1eca18c7a1a03e0
Mark Fine <mark.fine@gmail.com>
2015-12-09 14:59:23 -0800

    Start a skeleton.

--- haskell/src/SwiftNav/SBP/RTCM3.hs
+++ haskell/src/SwiftNav/SBP/RTCM3.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE LambdaCase #-}
+
 -- |
 -- Module:      SwiftNav.SBP.RTCM3
 -- Copyright:   Copyright (C) 2015 Swift Navigation, Inc.
@@ -16,5 +18,23 @@ import BasicPrelude
 import Data.RTCM3
 import SwiftNav.SBP
 
+fromMsgObs :: MsgObs -> Msg1002
+fromMsgObs _obs = Msg1002
+  { _msg1002_header = undefined
+  , _msg1002_observations = undefined
+  }
+
+fromMsgBasePosEcef :: MsgBasePosEcef -> Msg1005
+fromMsgBasePosEcef _basePosEcef = Msg1005
+  { _msg1005_reference = undefined
+  }
+
 convert :: SBPMsg -> Maybe RTCM3Msg
-convert = undefined
+convert = \case
+  (SBPMsgObs msg _sbp) -> Just $
+    RTCM3Msg1002 msg' $ toRTCM3 msg' where
+      msg' = fromMsgObs msg
+  (SBPMsgBasePosEcef msg _sbp) -> Just $
+    RTCM3Msg1005 msg' $ toRTCM3 msg' where
+      msg' = fromMsgBasePosEcef msg
+  _sbpMsg -> Nothing
