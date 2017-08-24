f86cf4d168d82dcb453d1fdda4f7b8bf888c8221
Mark Fine <mark.fine@gmail.com>
2017-08-23 17:28:51 -0700

    Add more test coverage.

--- haskell/test/Test/Data/RTCM3/SBP/Time.hs
+++ haskell/test/Test/Data/RTCM3/SBP/Time.hs
@@ -14,12 +14,20 @@ import SwiftNav.SBP
 testRolloverGpsTime :: TestTree
 testRolloverGpsTime =
   testGroup "Rollover GPS time tests"
-    [ testCase "No rollover" $
-        rolloverTowGpsTime 510191000 (GpsTimeNano 510191000 0 1961) @?= (GpsTimeNano 510191000 0 1961)
-    , testCase "Positive rollover" $
-        rolloverTowGpsTime 0 (GpsTimeNano 604800000 0 1961) @?= (GpsTimeNano 0 0 1962)
-    , testCase "Negative rollover" $
+    [ testCase "No rollover" $ do
+        rolloverTowGpsTime 0 (GpsTimeNano 0 0 1960) @?= (GpsTimeNano 0 0 1960)
+        rolloverTowGpsTime 302400000 (GpsTimeNano 302400000 0 1960) @?= (GpsTimeNano 302400000 0 1960)
+        rolloverTowGpsTime 604800000 (GpsTimeNano 604800000 0 1960) @?= (GpsTimeNano 604800000 0 1960)
+        rolloverTowGpsTime 0 (GpsTimeNano 302400000 0 1960) @?= (GpsTimeNano 0 0 1960)
+        rolloverTowGpsTime 302400000 (GpsTimeNano 604800000 0 1960) @?= (GpsTimeNano 302400000 0 1960)
+    , testCase "Positive rollover" $ do
+        rolloverTowGpsTime 0 (GpsTimeNano 302400001 0 1960) @?= (GpsTimeNano 0 0 1961)
+        rolloverTowGpsTime 0 (GpsTimeNano 604800000 0 1960) @?= (GpsTimeNano 0 0 1961)
+        rolloverTowGpsTime 302399999 (GpsTimeNano 604800000 0 1960) @?= (GpsTimeNano 302399999 0 1961)
+    , testCase "Negative rollover" $ do
         rolloverTowGpsTime 604800000 (GpsTimeNano 0 0 1961) @?= (GpsTimeNano 604800000 0 1960)
+        rolloverTowGpsTime 604800000 (GpsTimeNano 302399999 0 1961) @?= (GpsTimeNano 604800000 0 1960)
+        rolloverTowGpsTime 302400001 (GpsTimeNano 0 0 1961) @?= (GpsTimeNano 302400001 0 1960)
     ]
 
 tests :: TestTree
