cdfaf2dc5e2190bce82a411b9f9e0a36a80648c2
Mark Fine <mark.fine@gmail.com>
2017-08-25 10:26:43 -0700

    Comments.

--- haskell/src/Data/RTCM3/Replay.hs
+++ haskell/src/Data/RTCM3/Replay.hs
@@ -5,7 +5,7 @@ module Data.RTCM3.Replay
   ( replay
   ) where
 
-import BasicPrelude                hiding (filter, map)
+import BasicPrelude
 import Control.Concurrent          hiding (yield)
 import Control.Lens
 import Data.Conduit
@@ -23,12 +23,16 @@ toTow t = floor since
     begin      = addDays (-1) $ fromWeekDate y w 1
     since      = 1000 * diffUTCTime t (UTCTime begin 0)
 
+-- | Produce current GPS time of week.
+--
 currentTow :: MonadIO m => m Word32
 currentTow =
   liftIO $ toTow . addUTCTime (fromIntegral gpsLeapSeconds) <$> getCurrentTime
   where
     gpsLeapSeconds = 18 :: Int
 
+-- | Update time of week on replayed messages to be current.
+--
 postDelay :: MonadIO m => Conduit RTCM3Msg m RTCM3Msg
 postDelay = awaitForever $ \case
   (RTCM3Msg1002 m _rtcm3) -> do
@@ -42,6 +46,8 @@ postDelay = awaitForever $ \case
   _rtcm3Msg ->
     pure ()
 
+-- | Delay between two time of week values - differences over 15 seconds are ignored.
+--
 delayTow :: MonadIO m => Word32 -> Word32 -> m ()
 delayTow tow tow' =
   when (tow' > tow) $ do
@@ -51,17 +57,23 @@ delayTow tow tow' =
   where
     diffMilliseconds = 15 * 1000
 
+-- | Delay between observations.
+--
 delay :: MonadIO m => Conduit (Word32, RTCM3Msg) m RTCM3Msg
 delay = awaitForever $ uncurry $ \tow -> \case
   (RTCM3Msg1002 m rtcm3) -> peek >>= delayTow tow . fromMaybe maxBound . (fst <$>) >> yield (RTCM3Msg1002 m rtcm3)
   (RTCM3Msg1004 m rtcm3) -> peek >>= delayTow tow . fromMaybe maxBound . (fst <$>) >> yield (RTCM3Msg1004 m rtcm3)
   _rtcm3Msg              -> pure ()
 
+-- | Setup observations with their time of weeks.
+--
 preDelay :: Monad m => Conduit RTCM3Msg m (Word32, RTCM3Msg)
 preDelay = awaitForever $ \case
   (RTCM3Msg1002 m rtcm3) -> yield (m ^. msg1002_header ^. gpsObservationHeader_tow, RTCM3Msg1002 m rtcm3)
   (RTCM3Msg1004 m rtcm3) -> yield (m ^. msg1004_header ^. gpsObservationHeader_tow, RTCM3Msg1004 m rtcm3)
   _rtcm3Msg              -> pure ()
 
+-- | Replay observations.
+--
 replay :: MonadIO m => Conduit RTCM3Msg m RTCM3Msg
 replay = preDelay =$= delay =$= postDelay
