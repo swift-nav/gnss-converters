8b9ce5bc6a1ecc867a976949452492378b9bf74d
Mark Fine <mark.fine@gmail.com>
2018-12-12 16:47:51 -0800

    Standalone SBP converter (#163)
    Self-contained SBP converter for use by external code.

---
+++ c/include/gnss-converters/sbp_conv.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2018 Swift Navigation Inc.
+ * Contact: Swift Navigation <dev@swiftnav.com>
+ *
+ * This source is subject to the license found in the file 'LICENSE' which must
+ * be be distributed together with this source. All other rights reserved.
+ *
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#ifndef GNSS_CONVERTERS_SBP_CONV_INTERFACE_H
+#define GNSS_CONVERTERS_SBP_CONV_INTERFACE_H
+
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef struct sbp_conv_s *sbp_conv_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+sbp_conv_t sbp_conv_new();
+
+void sbp_conv_delete(sbp_conv_t conv);
+
+size_t sbp_conv(sbp_conv_t conv, uint16_t sender, uint16_t type,
+                uint8_t *rbuf, size_t rlen, uint8_t *wbuf, size_t wlen);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* GNSS_CONVERTERS_SBP_CONV_INTERFACE_H */
---
+++ c/src/sbp_conv.c
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2017 Swift Navigation Inc.
+ * Contact: Swift Navigation <dev@swiftnav.com>
+ *
+ * This source is subject to the license found in the file 'LICENSE' which must
+ * be be distributed together with this source. All other rights reserved.
+ *
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#include <time.h>
+#include <assert.h>
+#include <gnss-converters/rtcm3_sbp.h>
+#include <gnss-converters/sbp_conv.h>
+#include <swiftnav/fifo_byte.h>
+#include <swiftnav/gnss_time.h>
+#include <libsbp/sbp.h>
+
+struct sbp_conv_s {
+  struct rtcm3_out_state state;
+  fifo_t fifo;
+  uint8_t buf[2048];
+};
+
+static void sbp_conv_cb(uint8_t *buf, uint16_t len, void *context) {
+  fifo_t *fifo = context;
+  assert(fifo != NULL);
+  fifo_write(fifo, buf, len);
+}
+
+sbp_conv_t sbp_conv_new() {
+  sbp_conv_t conv = malloc(sizeof(struct sbp_conv_s));
+  if (conv != NULL) {
+    fifo_init(&conv->fifo, conv->buf, sizeof(conv->buf));
+    sbp2rtcm_init(&conv->state, sbp_conv_cb, &conv->fifo);
+    gps_time_t gps_time = time2gps_t(time(NULL));
+    sbp2rtcm_set_leap_second(get_gps_utc_offset(&gps_time, NULL), &conv->state);
+  }
+  return conv;
+}
+
+void sbp_conv_delete(sbp_conv_t conv) {
+  free(conv);
+}
+
+size_t sbp_conv(sbp_conv_t conv, uint16_t sender, uint16_t type,
+                uint8_t *rbuf, size_t rlen, uint8_t *wbuf, size_t wlen) {
+  switch (type) {
+  case SBP_MSG_BASE_POS_ECEF: {
+    sbp2rtcm_base_pos_ecef_cb(sender, rlen, rbuf, &conv->state);
+    break;
+  }
+  case SBP_MSG_OBS: {
+    sbp2rtcm_sbp_obs_cb(sender, rlen, rbuf, &conv->state);
+    break;
+  }
+  default: {
+    break;
+  }
+  }
+  return fifo_read(&conv->fifo, wbuf, wlen);
+}
