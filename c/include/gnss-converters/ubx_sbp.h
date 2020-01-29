a8b1ce7c917bbdb1072c4d4abbe7b282868ffdf8
Adel Mamin <adel.mamin@exafore.com>
2020-01-29 10:18:33 +0200

    Reverted year update in ubx_sbp.h

--- c/include/gnss-converters/ubx_sbp.h
+++ c/include/gnss-converters/ubx_sbp.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019,2020 Swift Navigation Inc.
+ * Copyright (C) 2019 Swift Navigation Inc.
  * Contact: Swift Navigation <dev@swiftnav.com>
  *
  * This source is subject to the license found in the file 'LICENSE' which must
